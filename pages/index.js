47a6ff7b43af6ca8599067c1574da9645599db10
yukplaySE <161188766+yukplaySE@users.noreply.github.com>
2024-02-25 13:55:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>YUKPLAY DAFTAR</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="YUKPLAY DAFTAR!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+<span><div class="GzjsW"><div class="k31gt"><h1>YUKPLAY</h1></div><div class="k31gt"><h2><strong>Apa itu YUKPLAY?</strong></h2></div><div class="k31gt"><p>YUKPLAY merupakan platform gaming online yang menyediakan berbagai macam permainan untuk dinikmati oleh para pengguna. Silahkan&nbsp;<a href="https://34.87.112.87/" target="_blank" rel="noopener">Daftar YUKPLAY&nbsp;</a>can dicoba pelayanan 24 jam lansung</p></div><div class="k31gt"><h2><strong>Varietas Game yang Ditawarkan</strong></h2></div><div class="k31gt"><p>YUKPLAY menawarkan berbagai genre game, mulai dari puzzle, petualangan, hingga game simulasi. Pengguna dapat memilih game sesuai dengan preferensi mereka dan menikmati pengalaman bermain yang unik dan menghibur.</p></div><div class="k31gt"><h2><strong>Komunitas dan Keterlibatan</strong></h2></div><div class="k31gt"><p>Salah satu aspek menarik dari YUKPLAY adalah adanya komunitas yang aktif dan ramah. Para pemain dapat berinteraksi, bertukar pengalaman, dan bahkan membentuk persahabatan baru melalui platform ini.</p></div><div class="k31gt"><h2><strong>Keamanan dan Tanggung Jawab</strong></h2></div><div class="k31gt"><p>Meskipun YUKPLAY menyediakan lingkungan bermain game yang aman, penting bagi pemain untuk tetap bertanggung jawab saat memainkan game tersebut.</p></div><div class="k31gt"><p>Dengan beragam permainan menarik dan lingkungan bermain yang aman, YUKPLAY menjadi pilihan tepat bagi Anda yang ingin menikmati bermain game. Jelajahi platform ini sekarang dan temukan kesenangan tanpa batas!</p></div><div class="k31gt"><h1><strong>FAQ</strong></h1></div><ol class="RihjV"><li class="k31gt"><strong>Apa jenis permainan yang tersedia di YUKPLAY?</strong>
+YUKPLAY menyediakan berbagai genre permainan, mulai dari puzzle hingga game simulasi.</li><li class="k31gt"><strong>Apakah ada batasan usia untuk mengakses YUKPLAY?</strong>
+Ya, YUKPLAY hanya tersedia untuk mereka yang berusia 18 tahun ke atas.</li><li class="k31gt"><strong>Apakah</strong>&nbsp;<strong>YUKPLAY dapat diakses di perangkat seluler?</strong>
+Ya, YUKPLAY dapat diakses melalui perangkat seluler dengan menggunakan aplikasi atau melalui browser web.</li><li class="k31gt"><strong>Bagaimana cara melaporkan perilaku yang tidak pantas di YUKPLAY?</strong>
+Jika Anda menemui perilaku yang tidak pantas, Anda dapat melaporkannya kepada tim dukungan YUKPLAY melalui fitur pelaporan yang disediakan di platform.</li></ol></div></span>
+
       </main>
 
       <Footer />
